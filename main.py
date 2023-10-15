deea2d3027f8f57abd4927460f76a73ec7d351c5
BK <betamemo@BK.local>
2023-10-15 21:36:05 +0700

    check collide and finish

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 import time
 from turtle import Screen
-from player import Player
+
 from car_manager import CarManager
+from player import Player
 from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+screen.listen()
+
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+screen.onkeypress(fun=player.go_up, key='Up')
+screen.onkeypress(fun=player.go_down, key='Down')
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.move()
+
+    if car_manager.is_collide(player.pos()):
+        print('game over')
+        scoreboard.game_over()
+        game_is_on = False
+
+    if player.is_finish(player.ycor()):
+        scoreboard.level_up()  # increase level
+
+screen.exitonclick()
