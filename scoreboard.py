4e2fee8d771f1021dbd3f22bffdd5321114b6aac
BK <betamemo@BK.local>
2023-10-15 21:37:18 +0700

    check level up or game over

--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,21 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.level = 1
+        self.hideturtle()
+        self.penup()
+
+    def level_up(self):
+        self.clear()
+        self.level += 1
+        self.setposition(-280, 260)
+        self.write(f'Level: {self.level}', align='left', font=FONT)
+
+    def game_over(self):
+        self.setposition(0, 0)
+        self.write('Game Over', align='center', font=FONT)
